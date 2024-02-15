60a768609ca95dbebad738dbbe95c55124735b71
Daniel Bucayan <dpbucayan@student.apc.edu.ph>
2024-02-15 16:38:36 +0800

    edited header

--- ci4/app/Views/templates/header.php
+++ ci4/app/Views/templates/header.php
@@ -1,8 +1,25 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>CodeIgniter Tutorial</title>
+<link rel="stylesheet" href="new_styles.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>My Poetry Collection</title>
 </head>
 <body>
 
-    <h1><?= esc($title) ?></h1>
+<header>
+<div class="container">
+<h3>Hi! I'm Daniel Bucayan and this is</h3><br>
+<h1 class="intro-title">My Poetry Collection</h1>
+<nav>
+<ul class="nav">
+<li class="nav-buttons"><a href="index2.php">Home</li></a>
+<li class="nav-buttons"><a href="poems.php">Poems</li></a>
+<li class="nav-buttons"><a href="about.php">About</li></a>
+<li class="nav-buttons"><a href="suggestions.php">Suggestions</li></a>
+</ul>
+</nav>
+</div>
+</header>
+
+<body>
