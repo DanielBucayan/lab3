cba68fe8b7d25b0ed58eebdfb8ce575ed5172bd3
Daniel Bucayan <dpbucayan@student.apc.edu.ph>
2024-02-29 16:33:04 +0800

    modified css

--- ci4/app/Views/pages/poems.php
+++ ci4/app/Views/pages/poems.php
@@ -28,37 +28,6 @@ Now, because of you, an angel, everything is bright and so heavenly.
   </section>
 </main>
 
-<div class="poems-flex-container">
-  <section id="poems-read">
-  <h2 class="poem-title" onclick="togglePoem('endlessHorizon')">Endless Horizon</h2>
-      <pre class="poem-content" id="endlessHorizonPoemContent">
-Two sad souls played by destiny's cruel game.
-Made it seemed like everything's set to look great on frame.
-Helped both of them create memories that would always remind each other's names.
-Only to wash out every chances, and put out those burning flames. 
-
-The wind blew them apart, along with the clouds full of memories.
-Fearing that everything would be erased and replaced by harsh realities,
-He decided to write every magic that he thought was only possible in fantasies.
-But it rained on his diary as he writes down the last part, recollecting the torn
-pages within his libraries. 
-
-As he stare and read what he wrote once more,
-He decided to just let destiny win, as the last page he tore.
-The page was brought by the wind to the other side of the shore.
-That's where a woman caught it and was able to read it, dropping with her knees
-on the floor. 
-
-"From the stars to the sunset at the horizon, where the sky meets the sea.
-You've somehow resembled everything that I wished to gaze and see,
-But you've always been far away from me. Maybe that's how it's meant to be.
-And that I can only long for you and wait 'til an apple grows in an orange tree."
-      </pre>
-    </div>
-  </section>
-</main>
-
-
 <main class="poem-container">
   <div>
     <h2 class="poem-title" onclick="togglePoem('silentThoughts')">Silent Thoughts</h2>
@@ -201,7 +170,6 @@ And that I can only long for you and wait 'til an apple grows in an orange tree.
     </div>
   </section>
 </main>
-
 
 <script>
   function togglePoem(poemId) {
