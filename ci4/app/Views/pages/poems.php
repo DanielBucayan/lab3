73306d811d2212068402e9683d57ba7ca58853bc
Daniel Bucayan <dpbucayan@student.apc.edu.ph>
2024-02-29 16:31:34 +0800

    modified css

--- ci4/app/Views/pages/poems.php
+++ ci4/app/Views/pages/poems.php
@@ -28,8 +28,7 @@ Now, because of you, an angel, everything is bright and so heavenly.
   </section>
 </main>
 
-<div class="right-side-poems">
-<main class="poem-container">
+<div class="poem-flex-container">
   <section id="poems-read">
   <h2 class="poem-title" onclick="togglePoem('endlessHorizon')">Endless Horizon</h2>
       <pre class="poem-content" id="endlessHorizonPoemContent">
